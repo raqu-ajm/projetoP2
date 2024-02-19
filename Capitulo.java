4314e5c219eb8fe4a2970200a7fdbc4ebe28ac91
raqu-ajm <149118362+raqu-ajm@users.noreply.github.com>
2024-02-19 18:53:35 -0300

    New 4

--- Capitulo.java
+++ Capitulo.java
@@ -1,106 +1,47 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-// Construtores
+
 public class Capitulo {
     private String nome;
     private String texto;
-    private String escolha1;
-    private String escolha2;
+    private ArrayList<String> escolhas = new ArrayList<String>();
     private Personagem personagem;
-    private int energia;
     private int alteracaoEnergia;
-    private Scanner leitor;
-    private int dano;
     
-    public Capitulo(String nome, String texto, String escolha1, String escolha2, Personagem personagem, int alteracaoEnergia, Scanner leitor) {
+    public Capitulo(String nome, String texto, Personagem personagem, int alteracaoEnergia) {
         this.nome = nome;
         this.texto = texto;
-        this.escolha1 = escolha1;
-        this.escolha2 = escolha2;
         this.personagem = personagem;
-
         this.alteracaoEnergia = alteracaoEnergia;
-        this.leitor = leitor;
+    }
+
+    public void adicionarEscolha (String escolha){
+        escolhas.add(escolha);
     }
 
     public void mostrar() {
         System.out.println("Capítulo: " + nome);
         System.out.println(texto);
-        System.out.println("Escolha 1: " + escolha1);
-        System.out.println("Escolha 2: " + escolha2);
-        System.out.println("Energia do Personagem: " + personagem.getEnergia());
+        this.personagem.mudarEnergia(alteracaoEnergia);
     }
 
-
-    public void alterarEnergia(int alteracao) {
-        this.energia += alteracao;
-    }
-
-    public int escolher() {
-        int escolha = 0;
-        do {
-            System.out.print("Escolha 1 ou 2: ");
-            String resposta = leitor.nextLine();
-            if (resposta.equals("1") || resposta.equals("2")) {
-                escolha = Integer.parseInt(resposta);
-            } else {
-                System.out.println("Escolha inválida. Tente novamente.");
+    public int escolher(Scanner leitor) {
+        while(true){
+            for (int i=0 ; i<this.escolhas.size() ;i++){
+                System.out.println("escolha numero " + (i+1) + ": " + this.escolhas.get(i));
             }
-        } while (escolha == 0);
-        
-        // Atualiza a energia do personagem de acordo com a alteração definida no capítulo
-        personagem.alterarEnergia(alteracaoEnergia);
-        return escolha;
-    }
-        
-        public int percaEnergia(){
-            return dano;
+
+            System.err.println("digite o numero da sua escolha:");
+            int respostaFinal = leitor.nextInt();
+
+                if (respostaFinal <= this.escolhas.size() && respostaFinal > 0){
+                    return respostaFinal;
+                }
+                else{
+                    continue;
+                }
+            }
+            
+
         }
-    
-
-    public static void main(String[] args) {
-        // Criar um objeto Scanner para entrada do usuário
-        Scanner entrada = new Scanner(System.in);
-
-        // Criar personagem
-        Personagem personagem = new Personagem("João",70 );
-
-        // Criar capítulos
-        Capitulo capitulo1 = new Capitulo("Capítulo 1", "Texto do Capitulo", "Escolha A", "Escolha B", personagem, -10, entrada);
-        Capitulo capitulo2 = new Capitulo("Capítulo 2", "Texto do Capítulo 2", "Escolha C", "Escolha D", personagem, 15, entrada);
-
-        // Mostrar e escolher capítulo 1
-        capitulo1.mostrar();
-        int escolhaCapitulo1 = capitulo1.escolher();
-        System.out.println("Escolha do Capítulo 1: " + escolhaCapitulo1);
-
-        // Mostrar e escolher capítulo 2
-        capitulo2.mostrar();
-        int escolhaCapitulo2 = capitulo2.escolher();
-        System.out.println("Escolha do Capítulo 2: " + escolhaCapitulo2);
-
-        // Fechar o scanner
-        entrada.close();
     }
-}
-
-class Personagem {
-    private String nome;
-    private int energia;
-
-    public Personagem(String nome, int energia) {
-        this.nome = nome;
-        this.energia = energia;
-    }
-
-    public int getEnergia() {
-        return energia;
-    }
-
-    public void alterarEnergia(int alteracao) {
-        this.energia += alteracao;
-    }
-
-    //public void alterarEnergia(int alteracao) {
-    
-}
-
--- Personagem.java
+++ Personagem.java
@@ -1,54 +1,38 @@
 public class Personagem {
-    private String nome;
-    private int energia;
-    private int maxEnergia;
-    private StringBuilder historia;
-
-    public Personagem(String nome, int energia, int maxEnergia, String historiaInicial) {
-        this.nome = nome;
-        this.energia = energia;
-        this.maxEnergia = maxEnergia;
-        this.historia =  new StringBuilder(historiaInicial);
-    }
-
+        private String nome;
+        private int energia;
+        private int maxEnergia;
     
-
-    //métodos 
-    public String getNome() {
-        return nome;
-    }
-
-    public int getEnergia() {
-        return energia;
-    }
-
-    public int getMaxEnergia() {
-        return maxEnergia;
-    }
-
-    public void adicionarCapitulo(String capitulo) {
-        this.historia.append("\n").append(capitulo);
-    }
-
-    public void mostrarHistoria() {
-        System.out.println(nome + ": " + historia.toString());
-    }
-
-
-    public void setEnergia(int energia) {
-        if (energia > maxEnergia) {
-            this.energia = maxEnergia;
-        } else if (energia < 0) {
-            this.energia = 0;
-            System.out.println(nome + " ficou esgotado!");
-        } else {
+        public Personagem(String nome, int energia, int maxEnergia) {
+            this.nome = nome;
             this.energia = energia;
+            this.maxEnergia = maxEnergia;
+        }
+    
+        //métodos
+        public String getNome() {
+            return nome;
+        }
+    
+        public int getEnergia() {
+            return energia;
+        }
+
+        public void mudarEnergia(int dano) {
+            System.out.println(this.nome + " tinha " + this.energia + " energia");
+            int novaEnergia = this.energia-dano;
+
+            if(novaEnergia < maxEnergia && novaEnergia > 0){
+                this.energia = novaEnergia;
+                System.out.println("agora ele tem " + this.energia + " de energia") ;
+            }
+            else if (novaEnergia >= maxEnergia) {
+                this.energia = maxEnergia;
+                System.out.println("agora ele tem " + this.energia + " de energia");
+            }
+            else if (novaEnergia <= 0) {
+                this.energia = 0;
+                System.out.println(nome + "chegou a "+ this.energia +" e ficou esgotado!");
+            }
         }
     }
-    public static void main(String[] args){
-        Personagem personagem1 = new Personagem("João", 50, 50, "João é um jovem corajoso que adora um desafio");
-        Personagem personagem2 = new Personagem("Lobo Mau", 35, 40, "Adora atrapalhar os humanos que entram na floresta");
-        Personagem personagem3 = new Personagem("Bler", 70,70, "Uma bruxa que pode ser boa ou má dependendo do humor que ele estiver naquele dia");
-    }
-}
-
